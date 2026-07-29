60e563dd2cc70330eb1375f26c4a07547d8016a7
Padushi <luca.aiden@gmail.com>
2026-07-29 17:43:40 -0400

    fixed print_board() and allowed printing for both points of view

--- src/core/board/board_utils.rs
+++ src/core/board/board_utils.rs
@@ -1,24 +1,50 @@
 use super::board_core::Board;
 
-pub fn print_board(board: &Board, with_piece_symbols: bool, white_at_bottom: bool) {
+pub fn print_board(board: &Board, white_at_bottom: bool) {
     println!();
-    println!("    a   b   c   d   e   f   g   h");
-    println!("  +---+---+---+---+---+---+---+---+");
-    for i in 0..8 {
-        println!(
-            "{} | {} | {} | {} | {} | {} | {} | {} | {} | {}",
-            8 - i,
-            board.state[8 * i],
-            board.state[8 * i + 1],
-            board.state[8 * i + 2],
-            board.state[8 * i + 3],
-            board.state[8 * i + 4],
-            board.state[8 * i + 5],
-            board.state[8 * i + 6],
-            board.state[8 * i + 7],
-            8 - i,
-        );
-        println!("  +---+---+---+---+---+---+---+---+");
+
+    match white_at_bottom {
+        true => {
+            println!("    a   b   c   d   e   f   g   h");
+            println!("  +---+---+---+---+---+---+---+---+");
+            for i in 0..8 {
+                println!(
+                    "{} | {} | {} | {} | {} | {} | {} | {} | {} | {}",
+                    8 - i,
+                    board.state[63 - 8 * i - 7],
+                    board.state[63 - 8 * i - 6],
+                    board.state[63 - 8 * i - 5],
+                    board.state[63 - 8 * i - 4],
+                    board.state[63 - 8 * i - 3],
+                    board.state[63 - 8 * i - 2],
+                    board.state[63 - 8 * i - 1],
+                    board.state[63 - 8 * i],
+                    8 - i,
+                );
+                println!("  +---+---+---+---+---+---+---+---+");
+            }
+            println!("    a   b   c   d   e   f   g   h");
+        }
+        false => {
+            println!("    h   g   f   e   d   c   c   b   a");
+            println!("  +---+---+---+---+---+---+---+---+");
+            for i in 0..8 {
+                println!(
+                    "{} | {} | {} | {} | {} | {} | {} | {} | {} | {}",
+                    i + 1,
+                    board.state[8 * i + 7],
+                    board.state[8 * i + 6],
+                    board.state[8 * i + 5],
+                    board.state[8 * i + 4],
+                    board.state[8 * i + 3],
+                    board.state[8 * i + 2],
+                    board.state[8 * i + 1],
+                    board.state[8 * i],
+                    i + 1,
+                );
+                println!("  +---+---+---+---+---+---+---+---+");
+            }
+            println!("    h   g   f   e   d   c   c   b   a");
+        }
     }
-    println!("    a   b   c   d   e   f   g   h");
 }
