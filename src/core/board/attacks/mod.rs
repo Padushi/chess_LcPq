ee54c6123988d124cd81224552fd0baba59cebdf
Padushi <luca.aiden@gmail.com>
2026-08-01 21:00:27 -0400

    king and knight movegen tests

--- src/core/board/attacks/mod.rs
+++ src/core/board/attacks/mod.rs
@@ -4,6 +4,8 @@ pub mod magic;
 pub mod pawn;
 pub mod sliding;
 
+mod tests;
+
 pub const FILE_A: u64 = 0x0101010101010101;
 pub const FILE_H: u64 = 0x8080808080808080;
 pub const RANK_1: u64 = 0x00000000000000FF;
